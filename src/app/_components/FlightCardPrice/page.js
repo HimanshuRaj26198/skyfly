caf1ba62d7205efcd473f1cc47ab8a9bb1ef5509
himanshu26198 <himanshu26198@gmail.com>
2024-08-27 20:48:23 +0530

    fixed price missing

--- src/app/_components/FlightCardPrice/page.js
+++ src/app/_components/FlightCardPrice/page.js
@@ -3,14 +3,19 @@ import { useEffect, useState } from "react";
 import CountryList from "../../../../lib/countryFlag.json";
 
 const FlightCardPrice = ({ price, currency }) => {
-    const [country, setCountry] = useState(null);
+    const [country, setCountry] = useState({
+        "country": "United States",
+        "flag": "/assets/img/flag/united-states.png",
+        "currency": "USD",
+        "currencySymbol": "$"
+    });
 
     useEffect(() => {
-        if (document) {
+        if (localStorage.getItem("country") !== undefined) {
             setCountry(JSON.parse(localStorage.getItem("country")))
         }
     }, [])
-    return <>{country && <div className="text-dark fs-3 fw-bold lh-base">{country.currency}{country.currencySymbol}{price}</div>}</>
+    return <> <div className="text-dark fs-3 fw-bold lh-base">{country.currency}{country.currencySymbol}{price}</div></>
 
 }
 
